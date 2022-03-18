adbdd5d024ae6788292e6b5ea9631510c2076eea
ChitralekhaMiQ <101545145+ChitralekhaMiQ@users.noreply.github.com>
2022-03-18 23:52:23 +0530

    Revert "Added BookApi"

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -1,28 +1,30 @@
 package com.example.backend.controller;
 
-import antlr.StringUtils;
-import com.example.backend.entity.Book;
 import com.example.backend.entity.User;
 import com.example.backend.repository.BookRepository;
 import com.example.backend.repository.UserRepository;
-import jdk.net.SocketFlow;
 import org.hibernate.sql.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userrepository;
+    @Autowired
+    private BookRepository bookrepository;
 
     @PostMapping("/signUp")
     public User insertUsers(@RequestBody User user)
     {
         return userrepository.save(user);
     }
+
 
 }
--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -5,5 +5,4 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
 }
