b8ffe5924fd028a005827530693b2cdb3363e023
chitralekha_s <chitralekha.s@miqdigital.com>
2022-03-20 17:05:17 +0530

    Added delete book api

--- src/main/java/com/example/backend/controller/BookController.java
+++ src/main/java/com/example/backend/controller/BookController.java
@@ -3,12 +3,15 @@ package com.example.backend.controller;
 import com.example.backend.entity.Book;
 import com.example.backend.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -25,7 +28,15 @@ public class BookController {
         book.setImage(imageFile.getBytes());
         bookrepository.save(book);
         return bookrepository.findAll();
+    };
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteBookById(@PathVariable Integer id) {
+        Optional<Book> bookEntity = bookrepository.findById(id);
+        Book deleteBook = bookEntity.get();
+        bookrepository.delete(deleteBook);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    ;
+
 }
